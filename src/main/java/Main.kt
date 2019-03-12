c53b0b922ce786f4e339db3f7f804a5c21bcab89
luecy1 <kowaheno@gmail.com>
2019-03-13 01:03:44 +0900

    最初のコミット

---
+++ src/main/java/Main.kt
@@ -0,0 +1,29 @@
+import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.stage.Stage
+
+fun main(args: Array<String>) {
+    Application.launch(Main::class.java, *args)
+}
+
+class Main : Application() {
+
+    override fun start(primaryStage: Stage) {
+
+        try {
+            val resource = javaClass.getResource("sample.fxml")
+            val parent = FXMLLoader.load<Parent>(resource)
+
+            val scene = Scene(parent)
+            primaryStage.scene = scene
+
+            primaryStage.title = "hoge"
+            primaryStage.show()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+    }
+}
